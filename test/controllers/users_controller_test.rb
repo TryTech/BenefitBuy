e93435efe4ad02566c8f6ad882400dab2a530305
ItaloCobains <italobrandao.contato@gmail.com>
2024-08-15 17:34:35 -0300

    chore: Update routes.rb to include users resource for new and create actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+  test "should create user" do
+    assert_difference("User.count") do
+      post users_url, params: { user: { email: "test@test.test", password: "password", password_confirmation: "password" } }
+    end
+  end
 end
