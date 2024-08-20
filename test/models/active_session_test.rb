c5c8e7c5edc170b78cb24c0ed5217ed30b04aef8
ItaloCobains <italobrandao.contato@gmail.com>
2024-08-19 21:13:26 -0300

    chore: Add tests for ActiveSession model and update associations

--- test/models/active_session_test.rb
+++ test/models/active_session_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ActiveSessionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @active_session = active_sessions(:one)
+  end
+
+  test "should belong to user" do
+    assert_respond_to @active_session, :user
+  end
+
+  test "should have secure token" do
+    assert_respond_to @active_session, :remember_token
+  end
 end
