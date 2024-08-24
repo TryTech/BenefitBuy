4e628f3f7e9d81e07429c587a541deefd3036341
ItaloCobains <italobrandao.contato@gmail.com>
2024-08-24 18:15:42 -0300

    chore: Add tests for destroying active sessions and update ActiveSessionsControllerTest

--- test/controllers/active_sessions_controller_test.rb
+++ test/controllers/active_sessions_controller_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class ActiveSessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "destroy active session" do
+    login_as(@user)
+
+    active_session = @user.active_sessions.create
+
+    delete active_session_url(active_session)
+  end
+
+  test "destroy all active sessions" do
+    login_as(@user)
+
+    @user.active_sessions.create
+    @user.active_sessions.create
+
+    delete destroy_all_active_sessions_url
+  end
 end
