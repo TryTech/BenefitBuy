af3f5e3f5fb7024eb31bef44dd0e540f0f1260f8
ItaloCobains <italobrandao.contato@gmail.com>
2024-08-25 12:12:14 -0300

    chore: Update Order model with simplified status enum syntax

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
+  has_many :order_items, dependent: :destroy
+
   validates :name, presence: true
   validates :stock, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :price, presence: true, numericality: { greater_than: 0 }
