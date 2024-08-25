ee2358651865cccc02031ae51633cc28ca284cf4
ItaloCobains <italobrandao.contato@gmail.com>
2024-08-25 13:56:06 -0300

    feat: Update order status values in fixtures and tests
    The order status values in the fixtures and tests have been updated to use integers instead of strings. This change aligns with the simplified status enum syntax in the Order model.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -34,4 +34,42 @@ class ProductTest < ActiveSupport::TestCase
     @product.name = "New product"
     assert @product.save, "Did not save the product"
   end
+
+  test "should not save product with negative stock" do
+    @product.stock = -1
+    assert_not @product.save, "Saved the product with negative stock"
+  end
+
+  test "should not save product with zero price" do
+    @product.price = 0
+    assert_not @product.save, "Saved the product with zero price"
+  end
+
+  test "should not save product with negative price" do
+    @product.price = -1
+    assert_not @product.save, "Saved the product with negative price"
+  end
+
+  test "should save product with positive stock" do
+    @product.stock = 1
+    assert @product.save, "Did not save the product with positive stock"
+  end
+
+  test "should has many order items" do
+    assert_respond_to @product, :order_items
+  end
+
+  test "should belong to user" do
+    assert_respond_to @product, :user
+  end
+
+  test "should belong to category" do
+    assert_respond_to @product, :category
+  end
+
+  test "should order items dependent destroy" do
+    assert_difference("OrderItem.count", -1) do
+      @product.destroy
+    end
+  end
 end
