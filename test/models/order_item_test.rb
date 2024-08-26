2634ecc67b2ca8c8a259c3085aa70773d8e11ad8
ItaloCobains <italobrandao.contato@gmail.com>
2024-08-25 21:54:29 -0300

    feat: Add validations for Address model
    The Address model has been updated with validations for the street, city, state, postal code, and country fields. This ensures that all required address information is present before saving the record.

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class OrderItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @order_item = order_items(:one)
+  end
+
+  test "should belong to order" do
+    assert_not_nil @order_item.order
+  end
+
+  test "should belong to product" do
+    assert_not_nil @order_item.product
+  end
 end
