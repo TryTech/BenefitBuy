69121629bdf0ca696d93970de1498cae57ca2533
ItaloCobains <italobrandao.contato@gmail.com>
2024-08-28 14:59:43 -0300

    chore: Update navigation menu styles and add menu.svg icon

--- app/javascript/controllers/menu_mobile_controller.js
+++ app/javascript/controllers/menu_mobile_controller.js
@@ -2,9 +2,35 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="menu-mobile"
 export default class extends Controller {
-  static targets = [ "menu" ]
-  
+  static targets = ["menu"]
+
   toggle() {
-    this.menuTarget.classList.toggle('hidden')
+    console.log('toggle');
+
+    // Remover event listener anterior para evitar múltiplos anexos
+    this.menuTarget.removeEventListener('animationend', this._handleAnimationEnd);
+
+    if (this.menuTarget.classList.contains('hidden')) {
+      // Exibir o menu com a animação de entrada
+      this.menuTarget.classList.remove('hidden', 'animate__fadeOutUp');
+      this.menuTarget.classList.add('animate__animated', 'animate__fadeInDown');
+      console.log('fadeInDown');
+    } else {
+      // Esconder o menu com a animação de saída
+      this.menuTarget.classList.remove('animate__fadeInDown');
+      this.menuTarget.classList.add('animate__fadeOutUp');
+      console.log('fadeOutUp');
+
+      // Anexar o evento de animação para adicionar `hidden` após a animação
+      this.menuTarget.addEventListener('animationend', this._handleAnimationEnd.bind(this), { once: true });
+    }
+  }
+
+  _handleAnimationEnd() {
+    console.log('animationend');
+    if (this.menuTarget.classList.contains('animate__fadeOutUp')) {
+      this.menuTarget.classList.add('hidden');
+      console.log('hidden');
+    }
   }
 }
