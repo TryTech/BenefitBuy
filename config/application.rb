a1411eba20b89466930592e670ae8f3f08e18237
ItaloCobains <italobrandao.contato@gmail.com>
2024-09-11 09:19:16 -0300

    chore: Update default locale to Portuguese (Brazil) and add support for multiple locales

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module BenefitBuy
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.load_path += Dir[Rails.root.join("my", "locales", "*.{rb,yml}")]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :"pt-BR"
+    config.i18n.available_locales = [ :en, :"pt-BR" ]
   end
 end
